ebb5ed643c97437f8c057373c65ba3ad7416ebab
KoolmeisterT <tiina.koolmeister@tptlive.ee>
2020-11-16 21:02:57 +0200

    Homework3

---
+++ Homework3/Homework3/Detail.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Media;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Homework3
+{
+    [Activity(Label = "Detailid")]
+    public class viewDetails : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.details);
+            TextView Country = FindViewById<TextView>(Resource.Id.country);
+            TextView Cases = FindViewById<TextView>(Resource.Id.cases);
+            TextView Deaths = FindViewById<TextView>(Resource.Id.deaths);
+            TextView Recovered = FindViewById<TextView>(Resource.Id.recovered);
+
+
+            string Country_ = Intent.GetStringExtra("Country");
+            string Cases_ = Intent.GetStringExtra("Cases");
+            string Deaths_ = Intent.GetStringExtra("Deaths");
+            string Recovered_ = Intent.GetStringExtra("Recovered");
+
+            Country.Text = Country_;
+            Cases.Text = Cases_;
+            Deaths.Text = Deaths_;
+            Recovered.Text = Recovered_;
+        }
+    }
+}
---
+++ Homework3/Homework3/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections.Generic;
+using Android.Content;
+using System;
+
+namespace Homework3
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+
+    public class HomeScreen : ListActivity
+    {
+        string[] items;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            items = new string[] { "EE", "LV", "LT" };
+            ListAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, items);
+        }
+
+        async void GetCountryDetails(string SelectedCountry)
+        {
+            Country country = new Country();
+
+            string url = "https://covid19-api.org/api/status/" + SelectedCountry;
+
+            var handler = new System.Net.Http.HttpClientHandler();
+            System.Net.Http.HttpClient client = new System.Net.Http.HttpClient(handler);
+            string result = await client.GetStringAsync(url);
+            country = Newtonsoft.Json.JsonConvert.DeserializeObject<Country>(result);
+
+            Intent listView = new Intent(this, typeof(viewDetails));
+            listView.PutExtra("Country", country.country);
+            listView.PutExtra("Cases", country.cases);
+            listView.PutExtra("Deaths", country.deaths);
+            listView.PutExtra("Recovered", country.recovered);
+            StartActivity(listView);
+        }
+
+        protected override void OnListItemClick(ListView l, Android.Views.View v, int position, long id)
+        {
+            var SelectedCountry = items[position];
+            GetCountryDetails(SelectedCountry);
+        }
+    }
+
+    public class Country
+    {
+        public string country { get; set; }
+        public DateTime last_update { get; set; }
+        public int cases { get; set; }
+        public int deaths { get; set; }
+        public int recovered { get; set; }
+    }
+
+}
